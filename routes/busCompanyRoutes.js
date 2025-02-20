c586cd1df32810622a47ec63bbb3a69f66dd14bb
Lambouhg <viett9961@gmail.com>
2025-02-20 18:24:25 +0700

    code-company

--- routes/busCompanyRoutes.js
+++ routes/busCompanyRoutes.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 const busCompanyController = require('../controllers/busCompanyController');
 
+// 1. Lấy danh sách tuyến đường của một nhà xe
+router.get('/:companyId', busCompanyController.getRoutesByBusCompany);
 
-// Tạo nhà xe (Cần quyền admin)
-router.post('/bus-companies',busCompanyController.createBusCompany);
+// 2. Xem danh sách vé của một tuyến đường
+router.get('/route/:routeId', busCompanyController.getTicketsByRoute);
 
-// Thêm tuyến đường (Cần quyền admin)
-router.post('/routes', busCompanyController.createRoute);
+// 3. Chỉnh sửa thông tin tuyến đường (Cần quyền admin)
+router.put('/routes/:routeId', busCompanyController.updateRoute);
+
+// 4. Xóa vé của tuyến đường (Cần quyền admin)
+router.delete('/tickets/:ticketId', busCompanyController.deleteTicket);
+
+// // 5. Quản lý vé của tuyến đường (Tạo, Cập nhật, Xóa)
+// router.post('/routes/:routeId', busCompanyController.manageTickets);
 
 module.exports = router;
